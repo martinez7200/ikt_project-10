62b3fdfdbbb1d50fdfb9c44abbc8db151fdef0c8
mi <90605588+martinez7200@users.noreply.github.com>
2021-09-20 17:14:13 +0200

    Update elso_recept.py

--- elso_recept.py
+++ elso_recept.py
@@ -1,6 +1,6 @@
 
 def bundáskényer(cola_type):
-    print('A tojásokat egy mélyvtányérba ütjük, és villval felverjük.')
+    print('A tojásokat egy mélytányérba ütjük, és villával felverjük.')
     print('A kenyérszeleteket vagy félbe vágva vagy egészben belerakjuk és megforgatjuk a tojásban.')
     print('Hagyjuk, hogy egy kicsit megszívja magát tojással.')
     print('Egy serpenyőbe kevés olajat teszünk, majd ha felforrósodott, akkor beletesszük a kenyérszeleteket és mindkét oldalát szép barnára sütjük.')
