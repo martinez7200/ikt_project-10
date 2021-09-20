7a8adb26495480e2deaec6a48e0d2600295bf886
mi <90605588+martinez7200@users.noreply.github.com>
2021-09-20 10:29:29 +0200

    Add files via upload

---
+++ elso_recept.py
@@ -0,0 +1,9 @@
+
+def bundáskényer(cola_type):
+    print('A tojásokat egy mélyvtányérba ütjük, és villval felverjük.')
+    print('A kenyérszeleteket vagy félbe vágva vagy egészben belerakjuk és megforgatjuk a tojásban.')
+    print('Hagyjuk, hogy egy kicsit megszívja magát tojással.')
+    print('Egy serpenyőbe kevés olajat teszünk, majd ha felforrósodott, akkor beletesszük a kenyérszeleteket és mindkét oldalát szép barnára sütjük.')
+    print('Szolgáld fel ' + cola_type + 'val/vel.')
+
+bundáskényer('coca')
